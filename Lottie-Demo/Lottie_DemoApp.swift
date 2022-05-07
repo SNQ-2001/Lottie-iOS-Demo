4f022767c2eb52e115b0d553e801fcb9872e19b4
SNQ-2001 <taishin05109@gmail.com>
2022-05-07 15:03:07 +0900

    Initial Commit

---
+++ Lottie-Demo/Lottie_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lottie_DemoApp.swift
+//  Lottie-Demo
+//
+//  Created by 宮本大新 on 2022/05/07.
+//
+
+import SwiftUI
+
+@main
+struct Lottie_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
